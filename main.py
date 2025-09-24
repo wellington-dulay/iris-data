661913a50e21562ab738946b4f958442ee48cc53
wellington dulay <wellington-dulay@outlook.com>
2025-09-24 15:29:24 +1000

    convert classes as integer codes

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pandas as pd
 
-def import_data(path):
+def import_data():
     # Load the dataset
-    data = pd.read_csv(path, header=None)
+    data = pd.read_csv("iris-data/iris/iris.data", header=None)
+    # Convert 'Category' column to categorical type
+    data[4] = data[4].astype('category')
     return data
 
 def main():
-    data = import_data("iris-data/iris/iris.data")
+    data = import_data()
     print(data.head())
+
+if __name__ == "__main__":
+    main()
